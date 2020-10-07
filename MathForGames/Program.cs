55efe4f01396ae48b8bc74faa8376e6708afebcd
EthanJones19 <Ethanjones800@yahoo.com>
2020-10-07 17:10:35 -0500

    Added Actor Class and Scene Class

--- MathForGames/Program.cs
+++ MathForGames/Program.cs
@@ -31,10 +31,9 @@ namespace raygamecsharp
         public static int Main()
         {
             //Create a new instance of the game class
-            Game game = new Game();
+            
 
             //Call run to begin the game.
-            game.Run();
 
             return 0;
         }
