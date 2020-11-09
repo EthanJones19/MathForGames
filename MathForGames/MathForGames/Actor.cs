b62a4e3d835240c17df7bdddd56b31e0e06d2ba0
EthanJones <ethanjones800@yahoo.com>
2020-11-09 14:58:50 -0600

    Reset the files

---
+++ MathForGames/MathForGames/Actor.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using MathLibrary;
+using Raylib_cs;
+
+namespace MathForGames
+{
+    class Actor
+    {
+        protected char _icon = ' ';
+        protected Matrix3 _localTransform;
+        protected Vector2 _velocity;
+        protected Matrix3 _globalTransform;
+        protected Matrix3 _translation = new Matrix3();
+        protected Matrix3 _rotation = new Matrix3();
+        protected Matrix3 _scale = new Matrix3();
+        protected ConsoleColor _color;
+        protected Color _raycolor;
+        protected Actor _parent;
+        protected Actor[] _children = new Actor[0];
+        protected float _rotationAngle;
+        private float _collisionRadius;
+
+        public bool Started { get; private set; }
+
+        public Vector2 Forward
+        {
+            get
+            {
+                return new Vector2(_localTransform.m11, _localTransform.m21);
+            }
+            set
+            {
+                Vector2 lookPosition = LocalPosition + value.Normalized;
+                LookAt(lookPosition);
+            }
+        }
+
+        public Vector2 WorldPostion
+        {
+            get
+            {
+                return new Vector2(_globalTransform.m13, _globalTransform.m21);
+            }
+        }
+
+        
+        public Vector2 LocalPosition
+        {
+            get
+            {
+                return new Vector2(_localTransform.m13, _localTransform.m23);
+            }
+            set
+            {
+                _translation.m13 = value.X;
+                _translation.m23 = value.Y;
+            }
+        }
+
+        public Vector2 Velocity
+        {
+            get
+            {
+                return _velocity;
+            }
+            set
+            {
+                _velocity = value;
+            }
+        }
+
+        public Actor(float x, float y, char icon = ' ', ConsoleColor color = ConsoleColor.White)
+        {
+            _raycolor = Color.WHITE;
+            _icon = icon;
+            _localTransform = new Matrix3();
+            LocalPosition = new Vector2(x, y);
+            _velocity = new Vector2();
+            _color = color;
+        }
+
+        public Actor(float x, float y, Color rayColor, char icon = ' ', ConsoleColor color = ConsoleColor.White) 
+            : this(x,y,icon,color)
+        {
+            _raycolor = rayColor;
+        }
+
+        public void AddChild(Actor child)
+        {
+            Actor[] tempArray = new Actor[_children.Length + 1];
+
+            for (int i = 0; i < _children.Length; i++)
+            {
+                tempArray[i] = _children[i];
+            }
+
+            tempArray[_children.Length] = child;
+            _children = tempArray;
+            child._parent = this;
+        }
+
+        public bool RemoveChild(Actor child)
+        {
+            bool childRemoved = false;
+
+            if (child == null)
+                return false;
+
+            Actor[] tempArray = new Actor[_children.Length - 1];
+
+            int j = 0;
+            for (int i = 0; i <_children.Length; i++)
+            {
+                if (child != _children[i])
+                {
+                    tempArray[j] = _children[i];
+                    j++;
+                }
+                else
+                {
+                    childRemoved = true;
+                }
+
+            }
+
+            _children = tempArray;
+            child._parent = null;
+            return childRemoved;
+        }
+
+
+
+
+        public void SetTranslation(Vector2 position)
+        {
+            _translation.m13 = position.X;
+            _translation.m23 = position.Y;
+        }
+
+        public void SetRotation(float radians)
+        {
+            _rotationAngle = radians;
+            _rotation.m11 = (float)Math.Cos(radians);
+            _rotation.m21 = -(float)Math.Sin(radians);
+            _rotation.m12 = (float)Math.Sin(radians);
+            _rotation.m22 = (float)Math.Cos(radians);
+        }
+
+        public void Rotate(float radians)
+        {
+            _rotationAngle += radians;
+            SetRotation(_rotationAngle);
+        }
+
+        public void LookAt(Vector2 position)
+        {
+            Vector2 direction = (position - LocalPosition).Normalized;
+
+            float dotProd = Vector2.DotProduct(Forward, direction);
+            if (Math.Abs(dotProd) > 1)
+                return;
+            float angle = (float)Math.Acos(dotProd);
+
+            Vector2 perp = new Vector2(direction.Y, -direction.X);
+
+            float perpDot = Vector2.DotProduct(perp, Forward);
+
+            if (perpDot != 0)
+                angle *= -perpDot / Math.Abs(perpDot);
+
+            Rotate(angle);
+        }
+
+        public bool CheckCollision(Actor other)
+        {
+            return false;
+        }
+
+        public virtual void OnCollision(Actor other)
+        {
+
+        }
+
+        public void SetScale(float x, float y)
+        {
+            _scale.m11 = x;
+            _scale.m22 = y;
+        }
+
+        private void UpdateTransform()
+        {
+            _localTransform = _translation * _rotation * _scale;
+        }
+
+
+
+        private void UpdateFacing()
+        {
+            if (_velocity.Magnitude <= 0)
+                return;
+            Forward = Velocity.Normalized;
+        }
+
+        public virtual void Start()
+        {
+            Started = true;
+        }
+
+        public virtual void Update(float deltaTime)
+        {
+            UpdateFacing();
+            UpdateTransform();
+        }
+
+        public virtual void Draw()
+        {
+            Raylib.DrawLine(
+                (int)(LocalPosition.X * 32),
+                (int)(LocalPosition.Y * 32), 
+                (int)((LocalPosition.X + Forward.X) * 32), 
+                (int)((LocalPosition.Y + Forward.Y) * 32), 
+                Color.WHITE
+                );
+            Console.ForegroundColor = _color;
+
+            if(LocalPosition.X >= 0 && LocalPosition.X < Console.WindowWidth
+                && LocalPosition.Y >= 0 && LocalPosition.Y < Console.WindowHeight)
+            {
+                Console.SetCursorPosition((int)LocalPosition.X, (int)LocalPosition.Y);
+                Console.Write(_icon);
+            }
+
+            Console.ForegroundColor = Game.DefaultColor;
+        }
+
+        public virtual void End()
+        {
+            Started = false;
+        }
+    }
+}
---
+++ MathForGames/MathForGames/Player.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MathLibrary;
+using Raylib_cs;
+
+namespace MathForGames
+{
+    class Player : Actor
+    {
+        private float _speed = 1;
+        private Sprite _sprite;
+
+
+        public float Speed
+        {
+            get
+            {
+                return _speed;
+            }
+            set
+            {
+                _speed = value;
+            }
+        }
+
+        public Player(float x, float y, char icon = ' ', ConsoleColor color = ConsoleColor.White) 
+            : base(x, y, icon, color)
+        {
+            _sprite = new Sprite("Images/player.png");
+        }
+
+        public Player(float x, float y, Color rayColor, char icon = ' ', ConsoleColor color = ConsoleColor.White)
+            : base(x, y, rayColor, icon, color)
+        {
+            _sprite = new Sprite("Images/player.png");
+        }
+
+        public override void Update(float deltaTime)
+        {
+            int xDirection = -Convert.ToInt32(Game.GetKeyDown((int)KeyboardKey.KEY_A)) +
+                    Convert.ToInt32(Game.GetKeyDown((int)KeyboardKey.KEY_D));
+
+            int yDirection = -Convert.ToInt32(Game.GetKeyDown((int)KeyboardKey.KEY_W)) +
+                    Convert.ToInt32(Game.GetKeyDown((int)KeyboardKey.KEY_S));
+
+            Velocity = new Vector2(xDirection, yDirection);
+            Velocity = Velocity.Normalized * Speed;
+
+            base.Update(deltaTime);
+        }
+
+        public override void Draw()
+        {
+            _sprite.Draw(_localTransform);
+            base.Draw();
+        }
+
+
+
+
+
+    }
+}
